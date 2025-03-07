782a7a4bfa2d78a27fd7310be83c2d31d4951a0b
Esteban Cruz <cruzcr6@gmail.com>
2025-03-07 10:14:20 -0600

    Updated project 06_Cupckakes to add more validations and test unit

--- 06.Cupcakes_Navigation_Training/app/build.gradle.kts
+++ 06.Cupcakes_Navigation_Training/app/build.gradle.kts
@@ -67,7 +67,7 @@ android {
 
 dependencies {
 
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.runtime:runtime")
@@ -82,6 +82,12 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:${rootProject.extra["lifecycle_version"]}")
     // Dependency for Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.4")
+    // Testing dependencies
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.02.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.8")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.8.8")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
